b133a942f35e0eeecdfe5d7708896169873c82bd
Santx <151431163+devsantx@users.noreply.github.com>
2026-03-23 16:31:35 -0300

    feat(app): wire theme provider into app bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { AuthProvider } from "./context/AuthContext";
+import { ThemeProvider } from "./context/ThemeContext";
 import AppRouter from "./router/index";
 
 const rootEl = document.getElementById("root");
 if (rootEl) {
   ReactDOM.createRoot(rootEl).render(
     <React.StrictMode>
-      <AuthProvider>
-        <AppRouter />
-      </AuthProvider>
+      <ThemeProvider>
+        <AuthProvider>
+          <AppRouter />
+        </AuthProvider>
+      </ThemeProvider>
     </React.StrictMode>
   );
 }
